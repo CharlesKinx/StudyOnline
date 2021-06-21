65a5fedcf524609a444e978b33887d70eeffee63
CharlesKinx <CharlesKinx@outlook.com>
2021-06-21 09:13:28 +0800

    测试

--- studyonline_server/src/main/java/com/example/studyonline_server/StudyOnlineServerApplication.java
+++ studyonline_server/src/main/java/com/example/studyonline_server/StudyOnlineServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudyonlineServerApplication {
+public class StudyOnlineServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudyonlineServerApplication.class, args);
+        SpringApplication.run(StudyOnlineServerApplication.class, args);
     }
 
 }
