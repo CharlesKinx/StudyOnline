a4aa22deea8573456e81ee45d225b667c6277587
CharlesKinx <CharlesKinx@outlook.com>
2021-06-21 08:45:50 +0800

    创建仓库

---
+++ studyonline_server/src/test/java/com/example/studyonline_server/StudyonlineServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyonline_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyonlineServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
