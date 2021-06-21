29c138ffc97793a440acd45f199202b0555a3288
CharlesKinx <CharlesKinx@outlook.com>
2021-06-21 14:56:50 +0800

    底部导航栏的fragment显示

--- studyonline_client/app/src/main/java/com/example/studyonline_client/activity/MainPageActivity.java
+++ studyonline_client/app/src/main/java/com/example/studyonline_client/activity/MainPageActivity.java
@@ -1,16 +1,103 @@
 package com.example.studyonline_client.activity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.studyonline_client.R;
+import com.example.studyonline_client.fragment.CourseLiveFragment;
+import com.example.studyonline_client.fragment.FriendContactFragment;
+import com.example.studyonline_client.fragment.FriendListFragment;
+import com.example.studyonline_client.fragment.ScoreAnalysisFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPageActivity extends AppCompatActivity {
+
+    private CourseLiveFragment courseLiveFragment;
+    private FriendContactFragment friendContactFragment;
+    private FriendListFragment friendListFragment;
+    private ScoreAnalysisFragment scoreAnalysisFragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_item1:
+                    showFragment(R.id.navigation_item1);
+                    return true;
+                case R.id.navigation_item2:
+                    showFragment(R.id.navigation_item2);
+                    return true;
+                case R.id.navigation_item3:
+                    showFragment(R.id.navigation_item3);
+                    return true;
+                case R.id.navigation_item4:
+                    showFragment(R.id.navigation_item4);
+                    return true;
+            }
+            return false;
+        }
+    } ;
+
+
+    private void initView(){
+        courseLiveFragment = new CourseLiveFragment();
+        friendContactFragment = new FriendContactFragment();
+        friendListFragment = new FriendListFragment();
+        scoreAnalysisFragment = new ScoreAnalysisFragment();
+
+        FragmentTransaction  fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.content,courseLiveFragment).add(R.id.content,friendListFragment)
+                .add(R.id.content,friendContactFragment).add(R.id.content,scoreAnalysisFragment);
+        fragmentTransaction.hide(courseLiveFragment).hide(friendListFragment)
+                .hide(friendContactFragment).hide(scoreAnalysisFragment);
+
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        showFragment(R.id.navigation_item1);
+    }
+
+    private void showFragment(int item){
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        switch (item){
+            case R.id.navigation_item1:
+                fragmentTransaction.hide(friendListFragment).hide(scoreAnalysisFragment).hide(friendContactFragment);
+                fragmentTransaction.show(courseLiveFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+            case R.id.navigation_item2:
+                fragmentTransaction.hide(courseLiveFragment).hide(scoreAnalysisFragment).hide(friendContactFragment);
+                fragmentTransaction.show(friendListFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+            case R.id.navigation_item3:
+                fragmentTransaction.hide(courseLiveFragment).hide(scoreAnalysisFragment).hide(friendListFragment);
+                fragmentTransaction.show(friendContactFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+            case R.id.navigation_item4:
+                fragmentTransaction.hide(courseLiveFragment).hide(friendContactFragment).hide(friendListFragment);
+                fragmentTransaction.show(scoreAnalysisFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                break;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+        initView();
+        BottomNavigationView bottomNavigationView  = findViewById(R.id.bv);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
     }
 
 }
--- studyonline_client/app/src/main/java/com/example/studyonline_client/fragment/CourseLiveFragment.java
+++ studyonline_client/app/src/main/java/com/example/studyonline_client/fragment/CourseLiveFragment.java
@@ -1,6 +1,20 @@
 package com.example.studyonline_client.fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+import com.example.studyonline_client.R;
 
 public class CourseLiveFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_courselive,container,false);
+        return view;
+    }
 }
