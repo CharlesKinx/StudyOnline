ca324efbe222eca453b8d7021f6a963792d0c1a0
CharlesKinx <CharlesKinx@outlook.com>
2021-06-25 09:08:48 +0800

    完成教师端与学生端在登录时的角色选择

--- studyonline_client/app/src/main/java/com/example/studyonline_client/activity/LoginActivity.java
+++ studyonline_client/app/src/main/java/com/example/studyonline_client/activity/LoginActivity.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.studyonline_client.R;
+import com.example.studyonline_client.teacher.TeacherActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.btn_login)
     Button login;
@@ -29,26 +33,75 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.tx_login_resister)
     TextView register;
 
+    @BindView(R.id.checkbox_student)
+    CheckBox studentCheckBox;
+
+    @BindView(R.id.checkbox_teacher)
+    CheckBox teacherCheckBox;
+
+
+    private boolean teacher=false;
+    private boolean student=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.avtivity_login);
         ButterKnife.bind(this);
-        register.setOnClickListener(new View.OnClickListener() {
+
+        studentCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intent);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    student = true;
+                }else {
+                    student = false;
+                }
             }
         });
 
-        login.setOnClickListener(new View.OnClickListener() {
+        teacherCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,MainPageActivity.class);
-                startActivity(intent);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    teacher = true;
+                }else {
+                    teacher =false;
+                }
             }
         });
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+
 
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login:
+                if(!teacher&&!student){
+                    Toast.makeText(LoginActivity.this,"请选择你的身份",Toast.LENGTH_SHORT).show();
+                }else if(teacher&&student){
+                    Toast.makeText(LoginActivity.this,"只能选择一种身份",Toast.LENGTH_SHORT).show();
+                }else if(teacher){
+                    Intent intent = new Intent(LoginActivity.this, TeacherActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Intent intent = new Intent(LoginActivity.this, MainPageActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                break;
+
+            case R.id.tx_login_resister:
+                Intent intent1 = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent1);
+                finish();
+                break;
+        }
+    }
+
 }
