b90697f8b79695e5c3bda7812a2628cfb3ece598
CharlesKinx <CharlesKinx@outlook.com>
2021-07-04 17:11:00 +0800

    提交作业页面

---
+++ studyonline_client/app/src/main/java/com/example/studyonline_client/activity/WriteWorkActivity.java
@@ -0,0 +1,15 @@
+package com.example.studyonline_client.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.studyonline_client.R;
+
+public class WriteWorkActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_work);
+    }
+}
